9ec85e7e43fa8b437af161ca3388985e6c7051a7
frescante <frescante@users.noreply.github.com>
2018-02-15 15:34:28 -0400

    lab3

---
+++ binary.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+	
+	ofstream myfile {"abc.bin", ios::binary};
+	
+	if (myfile)
+		{
+			
+			myfile.write (reinterpret_cast<char*>(&a), sizeof(a));
+			myfile.write (reinterpret_cast<char*>(&b), sizeof(b));
+			myfile.write (reinterpret_cast<char*>(&c), sizeof(c));
+			
+			cout<<"file created";
+		}
+   else 
+		{
+			cout<<"error openning file";
+		}
+	
+	myfile.close();
+	
+   return 0;
+}
---
+++ text.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+	
+	ofstream myfile {"abc.txt"};
+	
+	if (myfile)
+		{
+			myfile <<a<<" "<<b<<" "<<c;
+			cout<<"file created";
+		}
+   else 
+		{
+			cout<<"error openning file";
+		}
+	
+	myfile.close();
+	
+   return 0;
+}
